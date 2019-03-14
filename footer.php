e567ef448434d96be4d9d2018b80851c15d1d7d0
Moses Kirathe <39177599+moses-kirathe@users.noreply.github.com>
2019-03-13 17:25:20 -0700

    vtu 3 - footer  (#8)
    * Initial Footer Code
    
    * Final footer changes

--- footer.php
+++ footer.php
@@ -6,6 +6,28 @@
  */
  ?>
 
-<?php wp_footer() ?>
+<html>
+<head>
+<!--<link rel="stylesheet" type="text/css" href="assets/css/style.css">-->
+<style><?php include 'style.css';?></style>
+</head>
+
+<body>
+<p>This is a footer</p>
+  <footer class="footer">
+    <div class="flex-container">
+          <div>
+            <img class="header-logo logo-border footer-logo" src="<?php echo get_header_image(); ?>" alt="YWCA logo" />
+          
+        <?php dynamic_sidebar('footer_text');?>
+        </div>
+        <p>
+          &copy <?php echo date("Y"); ?> Vancouver Tenants Union 2019. <a href="http://codethechange.ca"> Developed by Code the Change Foundation.</a>
+        </p>
+        </div>
+    </div>
+  </div>
+  </footer>
+  <?php wp_footer() ?>
 </body>
 </html>
